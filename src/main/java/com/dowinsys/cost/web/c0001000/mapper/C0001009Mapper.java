a8445304a52f303f36c4d125db70765ef75834bd
young <young@10.212.134.101>
2025-10-07 19:17:48 +0900

    com.dowinsys.cost.web.c0001000.mapper DwisCostApplication.java

---
+++ src/main/java/com/dowinsys/cost/web/c0001000/mapper/C0001009Mapper.java
@@ -0,0 +1,13 @@
+/**
+*	기준정보 > 카세트 정보
+*/
+package com.dowinsys.cost.web.c0001000.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository("com.dowinsys.cost.web.c0001000.mapper.C0001009Mapper")
+@Mapper
+public interface C0001009Mapper {
+
+}
