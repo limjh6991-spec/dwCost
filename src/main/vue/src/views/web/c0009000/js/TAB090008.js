9df398bc9822812625b32de5a46daa446af635b4
okkw1120 <okkw1120@gmail.com>
2025-12-05 15:21:24 +0900

    원가레포트 메뉴 구성

---
+++ src/main/vue/src/views/web/c0009000/js/TAB090008.js
@@ -0,0 +1,17 @@
+/**
+ * Tab090008 - 부서별 판매관리비 집계표
+ */
+
+const tab090008GridField = {
+  fields: [
+  ],
+  columns: [
+  ],
+  options: {
+    edit: { editable: false },
+    display: { fitStyle: 'evenFill' },
+    footer: { visible: true },
+  },
+};
+
+export default tab090008GridField;
