d0ebe7c60d275fda2afe57d83cebc99230f3528d
okkw1120 <okkw1120@gmail.com>
2025-12-09 08:56:19 +0900

    e15a4c3 커밋 내용 복원 (판매관리비 부서별 집계표)

--- src/main/vue/src/views/web/c0009000/C0009008.vue
+++ src/main/vue/src/views/web/c0009000/C0009008.vue
@@ -1,11 +1,11 @@
 /** * 판매관리비 집계표 (TAB 구조) */
 <template>
   <auth-tabs>
-    <!-- Tab 1 판매관리비 -->
+    <!-- Tab 1 부서별 집계표 -->
     <template #tab-content-TAB090008>
       <TAB090008 />
     </template>
-    <!-- Tab 2 제품별 손익계산서 -->
+    <!-- Tab 2 제품별 집계표 -->
     <template #tab-content-TAB090009>
       <TAB090009 />
     </template>
