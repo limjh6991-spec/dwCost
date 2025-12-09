b13b86140784791cfd2ee9c4b2e05c6071f1f1c5
limjh6991-spec <limjh6991@gmail.com>
2025-12-09 15:26:26 +0900

    판매관리비 집계표 임시커밋

--- src/main/vue/src/views/web/c0009000/js/TAB090009.js
+++ src/main/vue/src/views/web/c0009000/js/TAB090009.js
@@ -2,16 +2,32 @@
  * Tab090009 - 제품별 판매관리비 집계표
  */
 
-const tab090009GridField = {
+const { ValueType } = require('realgrid');
+
+const grid = {
+  options: {
+    checkBar: { visible: false },
+    copy: { enabled: true, singleMode: true },
+    display: { columnMovable: false, editItemMerging: true, fitStyle: 'even', emptyMessage: '조회된 데이터가 없습니다.', hscrollBar: true, showEmptyMessage: true },
+    edit: { editable: false },
+    footer: { visible: false },
+    paste: { enabled: false },
+    rowIndicator: { visible: true },
+    sorting: { enabled: true },
+    stateBar: { visible: false },
+    filtering: { enabled: true },
+    fixed: { colBarWidth: 1, colCount: 1 },
+  },
   fields: [
+    { fieldName: 'gubun', dataType: ValueType.TEXT },
+    { fieldName: '제조경비계획', dataType: ValueType.NUMBER },
+    { fieldName: 'z합계', dataType: ValueType.NUMBER },
   ],
   columns: [
+    { name: 'gubun', fieldName: 'gubun', width: '80', header: { text: '구분' }, autoFilter: true, styleName: 'tl' },
+    { name: '판매관리비계획', fieldName: '판매관리비계획', width: '80', header: { text: '판매관리비 계획' }, autoFilter: false, styleName: 'tr', numberFormat: '#,##0' },
+    { name: 'z합계', fieldName: 'z합계', width: '80', header: { text: '판매관리비 합계' }, autoFilter: false, styleName: 'tr', numberFormat: '#,##0' },
   ],
-  options: {
-    edit: { editable: false },
-    display: { fitStyle: 'evenFill' },
-    footer: { visible: true },
-  },
 };
 
-export default tab090009GridField;
+module.exports = grid;
