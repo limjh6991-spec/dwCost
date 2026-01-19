01e46de9694b6c81013bff8b258c75af2f7fce30
okkw1120 <okkw1120@gmail.com>
2026-01-19 12:35:23 +0900

    총원가-유상사급 합계 푸터 추가

--- src/main/vue/src/views/web/c0009000/js/C0009013.js
+++ src/main/vue/src/views/web/c0009000/js/C0009013.js
@@ -8,7 +8,7 @@ const grid = {
     copy: { enabled: true, singleMode: false },
     display: { columnMovable: false, editItemMerging: true, fitStyle: 'fill', emptyMessage: '조회된 데이터가 없습니다.', hscrollBar: true, showEmptyMessage: true },
     edit: { editable: true, columnEditableFirst: true, commitByCell: true, commitWhenLeave: true },
-    footer: { visible: false },
+    footer: { visible: true },
     header: { height: 40, showTooltip: true, tooltipEllipsisOnly: true },
     hideDeletedRows: true,
     paste: { enabled: true, checkReadOnly: true },
@@ -82,9 +82,9 @@ const grid = {
     { name: '상계금액', fieldName: '상계금액', width: '100', header: { text: '상계금액' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0' },
     { name: '수량', fieldName: '수량', width: '100', header: { text: '수량' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0' },
     { name: '최종결산월재고단가', fieldName: '최종결산월재고단가', width: '120', header: { text: '최종결산월재고단가'}, autoFilter: true, styleName: 'tr', numberFormat: '#,##0.0000' },
-    { name: 'inMonth', fieldName: 'inMonth', width: '120', header: { text: '투입수량' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0' },
+    { name: 'inMonth', fieldName: 'inMonth', width: '120', header: { text: '투입수량' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0', footer: { expression: 'sum', numberFormat: '#,##0', styleName: 'sum-footer1' } },
     { name: 'distRate', fieldName: 'distRate', width: '120', header: { text: '배부비율' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0.0000' },
-    { name: 'finalAmt', fieldName: 'finalAmt', width: '120', header: { text: '배부금액' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0' },
+    { name: 'finalAmt', fieldName: 'finalAmt', width: '120', header: { text: '배부금액' }, autoFilter: true, styleName: 'tr', numberFormat: '#,##0',footer: { expression: 'sum', numberFormat: '#,##0', styleName: 'sum-footer1' } },
   ],
 };
 
